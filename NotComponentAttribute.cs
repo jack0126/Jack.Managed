cf836530eda23fe9c040ce879d1abd0ebd357e9c
jack0126 <45025728+jack0126@users.noreply.github.com>
2021-10-31 23:57:53 +0800

    Add files via upload

---
+++ NotComponentAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jack.Managed
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+    public class NotComponentAttribute : Attribute
+    {
+    }
+}
